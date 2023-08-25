de322d60c5b11f6492fbd95eda3176eafe42893f
iyxddw <iyxddw@163.com>
2023-08-25 16:10:26 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,10 +2,12 @@
 
 # 验证密码
 sudo -v
-sudo apt update && apt upgrade
+sudo apt update && sudo apt upgrade
 # 安装软件包
 sudo apt install -y bc bison build-essential curl flex g++-multilib gcc-multilib \
                     git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev \
                     lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev \
                     libssl-dev libxml2 libxml2-utils lzop pngcrush schedtool \
                     squashfs-tools xsltproc zip zlib1g-dev unzip python2 cpio
+echo "输入你的内核源码仓库链接"
+read user_input
